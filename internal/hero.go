3d023e80769310398fa8a3af29f3063bf2c7eb6a
NiumXp <formigacomcaimbra@gmail.com>
2021-12-18 20:44:09 -0300

    hero doc

--- internal/hero.go
+++ internal/hero.go
@@ -1,12 +1,21 @@
 package internal
 
+// Hero represents a hero in the game.
 type Hero struct {
-	Power   uint8
-	Speed   uint8
+	// Power is the damage of a bomb.
+	Power uint8
+	Speed uint8
+	// Stamina is the level of stamina of the Hero.
 	Stamina uint8
-	Bombs   uint8
-	Range   uint8
+	// Bombs is the amount of bombs can be dropped.
+	Bombs uint8
+	// Range is the range of the bombs.
+	Range uint8
 
+	// Baterry is if the Hero has a baterry.
 	Baterry bool
-	Mana    bool
+	// Mana is if the Hero has the mana powerup.
+	Mana bool
 }
+
+func (h *Hero) CanMoveAt(instant uint) bool

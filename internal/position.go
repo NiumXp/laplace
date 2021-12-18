698cee196dd0d7a14b95846deca2e7497ef14fab
NiumXp <formigacomcaimbra@gmail.com>
2021-12-18 16:30:24 -0300

    add NewPosition and doc position.go

--- internal/position.go
+++ internal/position.go
@@ -2,8 +2,15 @@ package internal
 
 import "fmt"
 
+// Position represents a position in the game.
 type Position [2]uint
 
+// NewPosition creates a new position with the given x and y.
+func NewPosition(x, y uint) *Position {
+	return &Position{x, y}
+}
+
+// String returns a string representation of the position.
 func (p *Position) String() string {
 	return fmt.Sprintf("(%d, %d)", p[0], p[1])
 }
